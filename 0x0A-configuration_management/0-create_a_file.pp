5db060bbab2deab0a4c5ffa7b7e595203b002620
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-24 07:14:27 -0800

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Create a file tmp
 
 file { '/tmp/school':
-    path => '/tmp/school',
-    ensure => file,
-    mode => 0744,
-    owner => www-data,
-    group => www-data,
+    ensure  => file,
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => www-data,
+    group   => www-data,
     content => 'I love Puppet'
 }
