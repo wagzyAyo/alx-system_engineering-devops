88109868ac5e0b3cdb9bd05508069ec0e02a34c7
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-24 07:04:31 -0800

    create puppe file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Create a file tmp
+
+file { '/tmp/school':
+    path => '/tmp/school',
+    ensure => file,
+    mode => 0744,
+    owner => www-data,
+    group => www-data,
+    content => 'I love Puppet'
+}
