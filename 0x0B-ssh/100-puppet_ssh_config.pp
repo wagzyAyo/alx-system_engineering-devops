a3f3067daa8fcf5f52fae8e112287596de4a7d59
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-26 05:17:06 -0800

    puppet ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
-# ssh configuration file
-file_line {'Set alias name':
-    path => '/etc/ssh/ssh_config',
-    line => 'Host 54.236.43.72
-        HostName 54.236.43.72
-        User ubuntu
-        IdentifyFile ~/.ssh/school
-        passwordAuthentication no',
+# Set up server configuration with puppet
+file_line{'Set alias name':
+  path => '/etc/ssh/ssh_config',
+  line => 'Host 54.205.163.79
+    HostName 54.205.163.79
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no',
 }
