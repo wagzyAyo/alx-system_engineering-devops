f32cc2d16688a4ff69819cf8f5f6e527681bbf39
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-26 05:01:47 -0800

    puppet lint

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # ssh configuration file
 file_line {'Set alias name':
-    path  => '/etc/ssh/ssh_config',
-    line  => 'Host 54.236.43.72
+    path => '/etc/ssh/ssh_config',
+    line => 'Host 54.236.43.72
         HostName 54.236.43.72
         User ubuntu
         IdentifyFile ~/.ssh/school
