13b1f574d57063a1c0b4059dde292dc404cc56ec
Ayo <talktojmcvibes@gmail.com>
2024-01-02 07:06:41 -0800

    api data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module request data from 
+jsonplaceholder"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    end_point = "https://jsonplaceholder.typicode.com/"
+    id=sys.argv[1]
+    response = requests.get('{}users/{}'
+                            .format(end_point, id)).json()
+    name = response["name"]
+    print('Employee {} is done with tasks'.format(name), end="")
+
+    todos = requests.get('{}todos?userId={}'.
+                         format(end_point, id)).json()
+    complete = []
+    for todo in todos:
+        if todo['completed'] is True:
+            complete.append(todo)
+    print('({}/{}):'.format(len(complete), len(todos)))
+    for task in complete:
+        print('\t {}'.format(task['title']))
