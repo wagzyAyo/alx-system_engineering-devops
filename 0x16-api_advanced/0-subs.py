65b3b60dc3b447e486799f4a1ac7440fb7ef90a1
Ayo <talktojmcvibes@gmail.com>
2024-01-16 16:38:45 +0000

    0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
 """
-
-from requests import get
+import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    if subreddit is None or not isinstance(subreddit, str):
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+    dic = res.json()
+    if 'data' not in dic:
         return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
